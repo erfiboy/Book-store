91750ada2145c8c3babec38306890e046df4a470
arsalanfiroozi <arsalan.firoozi@gmail.com>
2022-02-16 22:01:51 +0330

    .

--- Front/src/component/Bookspec.js
+++ Front/src/component/Bookspec.js
@@ -9,6 +9,7 @@ const cookies = new Cookies();
 const Bookspec = () => {
     let [logstate, logupd] = useState('');
     let [incard, incardupd] = useState(false);
+    let [available, availableupd] = useState(false);
     let [res, resupd] = useState('');
     let { id } = useParams();
     let [des, desupd] = useState('');
@@ -16,7 +17,7 @@ const Bookspec = () => {
     useEffect(async () => {
         if (cookies.get('token') == undefined)
             logupd('false');
-        else{
+        else {
             logupd('true');
             incardupd(true)
         }
@@ -24,6 +25,10 @@ const Bookspec = () => {
             let ans = await fetch('http://localhost:1337/status?id=' + id);
             ans = await ans.json();
             desupd(ans['response']);
+            availableupd(true);
+            if ('true' != ans['response']['is_available']) {
+                availableupd(false);
+            }
             console.log(ans['response'])
         } catch (err) {
             desupd('No Connection!');
@@ -69,30 +74,33 @@ const Bookspec = () => {
                     <div className="col-sm-3">
                     </div>
                 </div>
-                <div className="row" style={{ marginBottom: '2%' }}>
-                    <div className="col-sm-2">
-                    </div>
-                    <div className="col-sm-4" style={{ color: 'white', direction: 'rtl', textAlign: 'right' }} >
-                        <div style={{ width: '100%', backgroundColor: '#00000000', color: 'white', border: '0', textAlign: 'center' }} className="form-control px-3">
-                            قیمت: {des['price']} ریال
+                {available &&
+                    <div className="row" style={{ marginBottom: '2%' }}>
+                        <div className="col-sm-2">
+                        </div>
+                        <div className="col-sm-4" style={{ color: 'white', direction: 'rtl', textAlign: 'right' }} >
+                            <div style={{ width: '100%', backgroundColor: '#00000000', color: 'white', border: '0', textAlign: 'center' }} className="form-control px-3">
+                                قیمت: {des['price']} ریال
+                            </div>
+                        </div>
+                        <div className="col-sm-4" style={{ display: 'flex', color: 'white', direction: 'rtl', textAlign: 'right' }} >
+
+                            <span style={{ color: 'white', width: '30%', display: 'flex' }} >
+                                <div className="form-control" style={{ border: '0', backgroundColor: '#00000000', color: 'white' }} >تعداد:</div>
+                                <input type="number" min="1" max="100" className="form-control" style={{ color: 'white' }} onChange={evt => { num = evt.target.value; }} />
+                            </span>
+                            <button onClick={add} style={{ width: '70%', marginRight: '1%' }} className="form-control btn btn-primary submit px-3">
+                                به سبد خرید اضافه کن!
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-heart-fill" viewBox="0 0 16 16" style={{ marginRight: '3%' }}>
+                                    <path d="M11.5 4v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5ZM8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1Zm0 6.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z" />
+                                </svg>
+                            </button>
+                        </div>
+                        <div className="col-sm-2">
                         </div>
                     </div>
-                    <div className="col-sm-4" style={{ display: 'flex', color: 'white', direction: 'rtl', textAlign: 'right' }} >
-                        <span style={{ color: 'white', width: '30%', display: 'flex' }} >
-                            <div className="form-control" style={{ border: '0', backgroundColor: '#00000000', color: 'white' }} >تعداد:</div>
-                            <input type="number" min="1" max="100" className="form-control" style={{ color: 'white' }} onChange={evt => { num = evt.target.value; }} />
-                        </span>
-                        <button onClick={add} style={{ width: '70%', marginRight: '1%' }} className="form-control btn btn-primary submit px-3">
-                            به سبد خرید اضافه کن!
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-heart-fill" viewBox="0 0 16 16" style={{ marginRight: '3%' }}>
-                                <path d="M11.5 4v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5ZM8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1Zm0 6.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z" />
-                            </svg>
-                        </button>
-                    </div>
-                    <div className="col-sm-2">
-                    </div>
-                </div>
-                {incard &&
+                }
+                {incard && available &&
                     <div className="row" style={{ marginBottom: '2%' }}>
                         <div className="col-sm-2">
                         </div>

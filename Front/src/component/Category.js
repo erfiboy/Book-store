2aea690d228f64bce3c85e93817e06debc1652b6
arsalanfiroozi <arsalan.firoozi@gmail.com>
2022-02-17 15:43:42 +0330

    .

--- Front/src/component/Category.js
+++ Front/src/component/Category.js
@@ -1,7 +1,7 @@
 import React, { memo, useEffect, useState, useCallback, } from 'react';
 import { useParams } from 'react-router-dom';
 import Parse from 'parse'
-import Logo from '../logo.png'
+import unkcat from '../Book.png'
 import {
   BrowserRouter,
   Routes,
@@ -20,18 +20,38 @@ const Writers = () => {
       console.log(ans);
       let t = [];
       ans.forEach((item, index) => {
-        t.push(
-          <div className="col-sm-3" style={{ display: 'flex' }}>
-            <div htmlType="button" style={{ border: '0', margin: '1px', width: '100%', borderRadius: '3px !important' }} className="form-control btn btn-primary submit px-3 bg-dark">
-              <Link to={"/category/" + item['name']}>
-                {item['name']}
-                <svg xmlns="http://www.w3.org/2000/svg" style={{ marginRight: '2%' }} width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                </svg>
-              </Link>
+        if (item['image'] != undefined)
+          t.push(
+            <div className="col-sm-3" style={{ display: 'flex' }}>
+              <div className="card">
+                <img className="card-img-top" src={'http://localhost:1337/' + item['image']} height='400px' alt="Card image cap" />
+                <div className="card-body bg-dark" style={{ textAlign: 'center' }}>
+                  <Link to={"/category/" + item['name']}>
+                    {item['name']}
+                    <svg xmlns="http://www.w3.org/2000/svg" style={{ marginRight: '2%' }} width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                      <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                    </svg>
+                  </Link>
+                </div>
+              </div>
             </div>
-          </div>
-        )
+          )
+        else
+          t.push(
+            <div className="col-sm-3" style={{ display: 'flex' }}>
+              <div className="card">
+                <img className="card-img-top" src={unkcat} height='400px' alt="Card image cap" />
+                <div className="card-body bg-dark" style={{ textAlign: 'center' }}>
+                  <Link to={"/category/" + item['name']}>
+                    {item['name']}
+                    <svg xmlns="http://www.w3.org/2000/svg" style={{ marginRight: '2%' }} width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                      <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                    </svg>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          )
       })
       upd(t);
     } catch {
--- Front/src/component/Writersspec.js
+++ Front/src/component/Writersspec.js
@@ -1,6 +1,6 @@
 import React, { memo, useEffect, useState, useCallback, } from 'react';
 import { useParams } from 'react-router-dom';
-import booktestimg from '../Book.jpg'
+import Noimage from '../Book.jpg'
 import Parse from 'parse'
 import Logo from '../logo.png'
 import {
@@ -21,21 +21,42 @@ const Writersspec = () => {
       console.log(ans);
       let t = [];
       ans['response'].forEach((item, index) => {
-        t.push(
-          <div className="col-sm-3">
-            <div className="card">
-              <img className="card-img-top" src={booktestimg} alt="Card image cap" />
-              <div className="card-body">
-                <h5 className="card-title">{item['name']}</h5>
-                <p className="card-text">قیمت: {item['price']}</p>
-                <p className="card-text">نویسنده: {item['author']}</p>
-                <p className="card-text">خلاصه: {item['summary']}</p>
-                <Link to={"/book/" + item['id']} className="btn btn-primary">اطلاعات بیشتر</Link>
+        if (item['image'] != undefined)
+          t.push(
+            <div className="col-sm-3">
+              <div className="card">
+                <img className="card-img-top" src={'http://localhost:1337/' + item['image']} height='400px' alt="Card image cap" />
+                <div className="card-body">
+                  <h5 className="card-title">{item['name']}</h5>
+                  <p className="card-text">قیمت: {item['price']}</p>
+                  <p className="card-text">نویسنده: {item['author']}</p>
+                  <p className="card-text">خلاصه: {item['summary']}</p>
+                  <Link to={"/book/" + item['id']} className="btn btn-primary">اطلاعات بیشتر</Link>
+                </div>
               </div>
             </div>
-          </div>
-        )
+          )
+        else
+          t.push(
+            <div className="col-sm-3">
+              <div className="card">
+                <img className="card-img-top" src={Noimage} height='400px' alt="Card image cap" />
+                <div className="card-body">
+                  <h5 className="card-title">{item['name']}</h5>
+                  <p className="card-text">قیمت: {item['price']}</p>
+                  <p className="card-text">نویسنده: {item['author']}</p>
+                  <p className="card-text">خلاصه: {item['summary']}</p>
+                  <Link to={"/book/" + item['id']} className="btn btn-primary">اطلاعات بیشتر</Link>
+                </div>
+              </div>
+            </div>
+          )
       })
+      if (ans['response'].length == 0) {
+        t.push(
+          <div style={{ textAlign: 'center' }}>هیچ کتابی از این نویسنده در دسترس نیست! منتظر بمانید!</div>
+        )
+      }
       upd(t);
     } catch {
       upd('No connection to backend!');

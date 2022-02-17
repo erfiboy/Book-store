98fddd6a8c890dfbfdbc6528a086d1e0cfe898ea
arsalanfiroozi <arsalan.firoozi@gmail.com>
2022-02-17 22:53:55 +0330

    .

--- admin/src/component/Dashboard.js
+++ admin/src/component/Dashboard.js
@@ -1,40 +1,55 @@
-import React from "react";
-import { Nav } from "react-bootstrap";
-import { withRouter } from "react-router";
+import { Form, Input, Button, Checkbox, Card, Spin, message } from 'antd';
+import React, { memo, useState, useCallback, } from 'react';
+import Parse from 'parse'
+import Logo from '../logo.png'
 import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from 'recoil';
 
-const Side = props => {
-
-
+const About = () => {
   return (
     <>
-
-      {/* <Nav className="col-md-12 d-none d-md-block bg-dark sidebar"> */}
-      <div id="mySidenav" class="sidenav bg-dark">
-        <Nav.Item style={{paddingBottom: '1%',paddingTop: '1%'}}>
-          <Link className="nav-link" to="/dashboard">داشبورد JefBook</Link>
-        </Nav.Item>
-        <Nav.Item style={{paddingBottom: '1%',paddingTop: '1%'}}>
-          <Link className="nav-link" to="/addauthor">نویسنده</Link>
-        </Nav.Item>
-        <Nav.Item style={{paddingBottom: '1%',paddingTop: '1%'}}>
-          <Link className="nav-link" to="/addcategory">دسته بندی ها</Link>
-        </Nav.Item>
-        <Nav.Item style={{paddingBottom: '1%',paddingTop: '1%'}}>
-          <Link className="nav-link" to="/addbook">کتاب ها</Link>
-        </Nav.Item>
-        {/* <Nav.Item style={{paddingBottom: '1%',paddingTop: '1%'}}>
-          <Link className="nav-link" to="/">Active</Link>
-        </Nav.Item> */}
+      {/* <h3 style={{ textAlign: 'center', marginTop: '30px'}}>About Us</h3> */}
+      <div style={{ padding: '2%', verticalAlign: 'middle', paddingTop: '10%' }}>
+        <div className="row" style={{direction: 'ltr'}}>
+          <div className="col-sm-2">
+          </div>
+          <div className="col-sm-3" style={{ borderRadius: '3px', textAlign: 'center' }}>
+            <img className="card-img-top bg-dark" src={Logo} alt="Card image cap" />
+          </div>
+          <div className="col-sm-6" style={{ direction: 'ltr', verticalAlign: 'center', textAlign: 'left' }}>
+            This website is implemented as the project of the designing web-pages course. <br />
+            Groups members:
+            <ul>
+              <li>
+                Erfan Nosrati
+              </li>
+              <li>
+                Amirhosein Javadi
+              </li>
+              <li>
+                Arsalan Firoozi
+              </li>
+            </ul> <br />
+            You can see books, search in books' list, and purchase.<br /><br />
+            <h4 style={{ position: 'relative', width: '50%', left: '25%' }}>
+              We design,<br />
+              We create,<br />
+              We are rich.
+            </h4>
+          </div>
+          <div className="col-sm-2">
+          </div>
+        </div>
       </div>
-      {/* </Nav> */}
+
     </>
   );
 };
 
-export default Side
+export default React.memo(About)
